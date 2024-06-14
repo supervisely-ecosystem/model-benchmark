c130bc29923b2e40b43bae5a01c60c5ff5c9df5a
Ars Vladimirov <grokhael@gmail.com>
2024-06-14 19:33:44 +0500

    optimize image switch speed

--- src/ui/_02_model_predictions.py
+++ src/ui/_02_model_predictions.py
@@ -106,28 +106,16 @@ def handle(grid_gallery, selected_image_name="000000575815.jpg"):
     diff_project_id = 38740
     diff_dataset_id = 92002
 
-    gt_project_meta = sly.ProjectMeta.from_json(data=g.api.project.get_meta(id=gt_project_id))
-    pred_project_meta = sly.ProjectMeta.from_json(data=g.api.project.get_meta(id=pred_project_id))
-    diff_project_meta = sly.ProjectMeta.from_json(data=g.api.project.get_meta(id=diff_project_id))
-
-    for image in g.api.image.get_list(dataset_id=gt_dataset_id):
-        if image.name == selected_image_name:
-            gt_image_info = image
-            break
-
-    for image in g.api.image.get_list(dataset_id=pred_dataset_id):
-        if image.name == gt_image_info.name:
-            pred_image_info = image
-            break
-
-    for image in g.api.image.get_list(dataset_id=diff_dataset_id):
-        if image.name == "000000575815.jpg":  # gt_image_info.name:
-            diff_image_info = image
-            break
+    gt_image_info = g.api.image.get_info_by_name(gt_dataset_id, selected_image_name)
+    pred_image_info = g.api.image.get_info_by_name(pred_dataset_id, selected_image_name)
+    diff_image_info = g.api.image.get_info_by_name(diff_dataset_id, "000000575815.jpg")
 
     images_infos = [gt_image_info, pred_image_info, diff_image_info]
     anns_infos = [g.api.annotation.download(x.id) for x in images_infos]
-    project_metas = [gt_project_meta, pred_project_meta, diff_project_meta]
+    project_metas = [
+        sly.ProjectMeta.from_json(data=g.api.project.get_meta(id=x))
+        for x in [gt_project_id, pred_project_id, diff_project_id]
+    ]
 
     for idx, (image_info, ann_info, project_meta) in enumerate(
         zip(images_infos, anns_infos, project_metas)
@@ -149,13 +137,6 @@ def handle_table_row(clicked_row: sly.app.widgets.FastTable.ClickedRow):
     grid_gallery.clean_up()
     handle(grid_gallery, clicked_row.row[0])
     grid_gallery.update_data()
-
-    # grid_gallery.get_table_row(table_model_preds.widget_id, clicked_row)
-
-    # sly.app.show_dialog(
-    #     f"{clicked_row.row[0]}",
-    #     f"You clicked table row with idx={clicked_row.row_index} in source data",
-    # )
 
 
 # Input card with all widgets.
