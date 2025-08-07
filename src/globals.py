4b7b64560565346cb99d46d07ea56c429ef071a1
vorozhkog <vorozhkog@icloud.com>
2025-08-07 16:40:07 +0100

    init

--- src/globals.py
+++ src/globals.py
@@ -1,9 +1,10 @@
 import os
 from distutils.util import strtobool
 
+import supervisely as sly
 from dotenv import load_dotenv
 
-import supervisely as sly
+from src.ui.compare import run_compare
 
 if sly.is_development():
     load_dotenv("local.env")
@@ -25,6 +26,11 @@ task_id = sly.env.task_id(raise_not_found=False)
 session_id = os.environ.get("modal.state.sessionId", None)
 if session_id is not None:
     session_id = int(session_id)
+eval_dirs = os.environ.get("modal.state.evalDirs", None)
+if eval_dirs is not None:
+    eval_dirs = eval_dirs.split(",")
+    run_compare(eval_dirs)
+
 session = None
 
 model_classes = None
