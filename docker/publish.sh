226ad91a52e769aec1c11844b91889591e341ec5
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2026-03-06 14:54:34 +0400

    Fix model comparison using collections (#23)

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/model-benchmark:0.0.1 . && \
-docker push supervisely/model-benchmark:0.0.1
+docker build -t supervisely/model-benchmark:1.1.1 . && \
+docker push supervisely/model-benchmark:1.1.1
