afbe14baba8a5d4f0d347068491952231a02c12c
cxnt <pavelbartsits@gmail.com>
2026-03-12 18:23:49 +0400

    Fix model benchmark comparison for projects with nested datasets

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/model-benchmark:1.1.1 . && \
-docker push supervisely/model-benchmark:1.1.1
+docker build -t supervisely/model-benchmark:1.1.2 . && \
+docker push supervisely/model-benchmark:1.1.2
